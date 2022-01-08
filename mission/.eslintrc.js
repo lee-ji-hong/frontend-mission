c362b119c914c2046d76c11e48f51abcdcc7f7c8
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-01-08 16:43:50 +0900

    fix: linebreak 오류 수정

--- mission/.eslintrc.js
+++ mission/.eslintrc.js
@@ -4,21 +4,22 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
+    "plugin:vue/vue3-essential", 
+    "@vue/airbnb"
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": 0,
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)", 
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true,
