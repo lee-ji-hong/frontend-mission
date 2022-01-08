33cd6bde4935a8593a0bb5592a0432dec86dec0b
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-01-09 02:07:32 +0900

    Feat: "추가 LeftRotateString component"

---
+++ mission/src/components/LeftRotateString.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrap-inuput-bar" >
+        <div class="input-bar">
+            <input type="text" v-model="inputText" />
+            <button>회전</button>
+            <button>클릭횟수</button>
+            <p>{{inputText}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    defaultText: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      inputText: this.defaultText,
+    };
+  },
+};
+</script>
--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LeftRotateString/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import LeftRotateString from '@/components/LeftRotateString.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    LeftRotateString,
   },
 };
 </script>
