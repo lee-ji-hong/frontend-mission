509a3c70f4e47628b67f5b15fcee7e7228e7ec86
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-01-09 13:30:49 +0900

    Test: "테스트 회전문자열"

--- mission/tests/unit/leftRotateString.spec.js
+++ mission/tests/unit/leftRotateString.spec.js
@@ -2,12 +2,18 @@ import { shallowMount } from '@vue/test-utils';
 import LeftRotateString from '@/components/LeftRotateString.vue';
 
 describe('LeftRotateString.vue', () => {
+  const defaultText = 'Projectlion';
+  const wrapper = shallowMount(LeftRotateString, {
+    props: { defaultText },
+  });
   it('input에서 입력한 값이 실시간으로 p에 update된다.', () => {
-    const defaultText = 'Projectlion';
-    const wrapper = shallowMount(LeftRotateString, {
-      props: { defaultText },
-    });
     const p = wrapper.find('p');
     expect(p.text()).toMatch(defaultText);
+  });
+
+  it('버튼 클릭 시 문자열 왼쪽으로 슬라이드', () => {
+    const rotateButton = wrapper.find('.rotate_btn');
+    rotateButton.trigger('click');
+    expect(wrapper.vm.inputText).toContain('rojectlionP');
   });
 });
