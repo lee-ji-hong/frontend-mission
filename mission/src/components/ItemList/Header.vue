dc9e6d35f45effe15dbb9390f1799df07b2f03a0
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-01-23 00:44:27 +0900

    Feat: "생성 Header component"

---
+++ mission/src/components/ItemList/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="item-list-header">
+    <h1>SEMININ</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+<style>
+header{
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  background-color:white;
+  box-shadow:0px 4px 9px rgb(239 239 242);
+}
+h1{
+  color: black;
+  font-size:28px;
+}
+</style>
