c5150a46abab8591848970f7547b70a11add89df
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-01-23 13:27:52 +0900

    Test: "테스트 item component 렌더링"

--- mission/tests/unit/ItemList/Item.spec.js
+++ mission/tests/unit/ItemList/Item.spec.js
@@ -7,4 +7,43 @@ describe('ItemListItem', () => {
 
     expect(wrapper.find('.item-list-item').exists()).toBe(true);
   });
+
+  it('item에서 price 렌더링 확인', () => {
+    const testPrice = 'test price';
+    const wrapper = mount(ItemInfoPage, {
+      data() {
+        return {
+          name: testPrice,
+        };
+      },
+    });
+
+    expect(wrapper.get('[data-test="test-price"]').text()).toBe(testPrice);
+  });
+
+  it('item에서 sellername 렌더링 확인', () => {
+    const testSellertName = 'test seller name';
+    const wrapper = mount(ItemInfoPage, {
+      data() {
+        return {
+          name: testSellertName,
+        };
+      },
+    });
+
+    expect(wrapper.get('[data-test="seller-name"]').text()).toBe(testSellertName);
+  });
+
+  it('item에서 name 렌더링 확인', () => {
+    const testProductName = 'test product name';
+    const wrapper = mount(ItemInfoPage, {
+      data() {
+        return {
+          name: testProductName,
+        };
+      },
+    });
+
+    expect(wrapper.get('[data-test="product-name"]').text()).toBe(testProductName);
+  });
 });
