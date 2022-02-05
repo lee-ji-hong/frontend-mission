d059cb05600272606365d1ac2d5ae99739106f60
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-02-06 02:17:40 +0900

    Test: "테스트 Header Using a Real Router "

--- mission/tests/unit/ItemList/header.spec.js
+++ mission/tests/unit/ItemList/header.spec.js
@@ -1,11 +1,38 @@
-import { mount } from '@vue/test-utils';
+import { flushPromises, mount } from '@vue/test-utils';
+import { createRouter, createWebHistory } from 'vue-router';
 import Header from '@/components/ItemList/Header.vue';
+import ItemListPage from '@/views/ItemList.vue';
+
+const routes = [
+  {
+    path: '/lsit',
+    component: ItemListPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
 describe('ItemListItem', () => {
-  it('redners ItemListItem', () => {
-    const wrapper = mount(Header);
+  it('header f렌더링 테스트', async () => {
+    router.push('/list');
 
+    await router.isReady();
+
+    const wrapper = mount(Header, {
+      global: {
+        plugins: [router],
+      },
+    });
+    expect(wrapper.find('.headbar').exists()).toBe(true);
     expect(wrapper.find('.item-list-header').exists()).toBe(true);
-  });
+    expect(wrapper.find('.headbar').exists()).toBe(true);
+    expect(wrapper.find('.headbar h1').exists()).toBe(true);
 
+    await wrapper.find('.headbar h1').trigger('click');
+    await flushPromises();
+    expect(wrapper.html()).toContain('SEMININ');
+  });
 });
