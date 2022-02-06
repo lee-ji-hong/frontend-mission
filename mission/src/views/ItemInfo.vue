5ac48654668987181bd6ad4c063ca21749a7f0f7
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-02-06 20:22:49 +0900

    Feat: "ItemInfo repository pattrn api 연동 및 필요없는 데이터 삭제 "

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,119 +1,162 @@
 <template>
-<div id="item-info-page">
-  <div class="detail__data" data-test="product-name">
-    <div class="detail__image">
-      <img alt="product_main" src="../assets/list_prev_btn.png" class="detail_prev_btn"/>
-      <img class="detail_main_image" :src="image" :alt="title">
+  <div class="detail-wrap">
+    <div class="detail__data">
+      <div class="detail__image">
+        <img class="detail_main_image" :src="item.image" >
+      </div>
+
+      <div class="detail__shop">
+        <div class="detail__shop_image">
+          <img :src="item.seller.profile_image" alt="">
+        </div>
+        <div class="detail__shop_name">
+          <h3>
+            <strong>{{ item.seller.name }}</strong>
+          </h3>
+          <p v-for="tag in item.seller.hash_tags" :key="tag">
+              #{{ tag }}
+          </p>
+        </div>
+        <div data-test="company-star" class="detail__shop_btn">
+          <span >
+          <i class="far fa-star w3-right"></i>
+        </span>
+        </div>
+      </div>
     </div>
-    <div class="detail__shop">
-      <div class="detail__shop_image">
-        <img class="detail_main_image" :src="seller.seller_id" :alt="profile_seller">
+
+      <div class="detail__actions">
+          <div>
+            <h3 data-test="name">{{ item.name }}</h3>
+          </div>
+          <div class="detail__actions_cost">
+            <div
+            class="detail__actions_cost_percent"
+            id="discount"
+            data-test="discount-rate"
+            v-if="this.item.original_price">
+              {{ display_discount_rate }}%
+            </div>
+            <div data-test="price" class="detail__actions_cost_price">
+              {{ priceWidthComma(item.price) }}원
+            </div>
+            <div class="detail__actions_cost_sale" v-if="this.item.original_price">
+              {{ priceWidthComma(item.original_price) }}원
+            </div>
+        </div>
+
+        <div data-test="product-detail" class="product-detail">
+          <p class="detail-title">상품 설명</p>
+          <div class="description_p" v-html="item.description"></div>
+        </div>
       </div>
-      <div class="detail__shop_name">
-        <h3>세미닌</h3>
-        <p>#페미닌 #러블리 #마켓 만족도 89%</p>
+
+      <div id="reviews">
+        <div
+          class="review w3-row"
+          v-for="review in reviews"
+          :key="review.review_no"
+        >
+          <div class="review-text">
+            <div class="review-row-1">
+              <div class="review-writer" data-test="review-writer">
+                {{ review.writer }}
+              </div>
+              <div class="review-created" data-test="review-created">
+                {{ review.created }}
+              </div>
+            </div>
+            <div class="review-title" data-test="review-title">
+              {{ review.title }}
+            </div>
+            <div class="review-content" data-test="review-content">
+              {{ review.content }}
+            </div>
+          </div>
+          <div
+            v-if="doesReviewImgExists(review)"
+            class="review-img"
+            data-test="review-img"
+          >
+            <img class="w3-right" :src="review.img" />
+          </div>
+        </div>
       </div>
-      <div class="detail__shop_btn">
-        <h3>하트</h3>
-        <!--<button @click="addToCart">Add to Cart</button>-->
+      <div id="footer">
+        <button
+          id="btn-pruchase"
+          class="w3-round-large"
+          data-test="footer-price"
+        >
+          {{ `${priceStringWithComma(price)} 구매` }}
+        </button>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
+import RepositoryFactory from '@/repositories/RepositoryFactory';
+
+const ItemRepository = RepositoryFactory.get('items');
+
 export default {
   name: 'ItemInfoPage',
+  props: {
+    id: { type: String, default: '' },
+  },
   data() {
     return {
-      product_id: 'p1',
-      image: 'https://shop-phinf.pstatic.net/20210906_19/1630913405061mrqAV_JPEG/32049247723857243_192935124.JPG?type=m510',
-      name: '[블랙컬러추가][엘레강스/고퀄set] 마벨 트위드 자켓 스커트 투피스 세트',
-      price: ' 42,000원',
-      original_price: '52,000원',
-      percent: '20%',
-      description: `<div class="description">
-      <div class="description_p">
-        <h3>주문 전, 꼭 필독해주세요!</h3><br>
-        이 공지를 읽지 않아 발생하는 모든 일들은<br>
-        세니룸에서책임지지 않습니다.<br>
-        (주문과 동시에 공지사항을 숙지한 것으로 간주합니다)
-      </div>
-      <div class="description_p">
-        ※세니룸 고객센터의 유선연결은 어려울때가 많으며,<br>
-        에이블리 문의하기로 문의글을 남겨주시면<br>
-        정확하고 빠른 답변 드리겠습니다 :) ※ <br>
-        세니룸 상담시간 낮 12:00~17:00 (주말 및 공휴일 제외)
-      </div>
-      <div class="description_p">
-        <h3>-주문취소-</h3><br>
-        세니룸은 오전 11시부터 송장출력을 시작하므로,<br>
-        주문취소의 경우 오전 11시 이전에 가능합니다.<br>
-        송장 출력이 시작되면 주문취소가 어려울 수 있습니다.
-      </div>
-      <div><img src="https://imgb.a-bly.com/data/editor/20210906_1630909585997907l.JPG" alt="title"></div>
-      <div><img src="https://imgb.a-bly.com/data/editor/20210906_1630909643610521l.JPG" alt="title"></div>
-      <div><img src="https://shop-phinf.pstatic.net/20210906_288/1630913435347F1sza_JPEG/32049277932462662_2034029400.JPG?type=f40" alt="title"></div>
-      </div>`,
-      seller: {
-        seller_id: 's1',
-        name: '세미닌',
-        hash_tags: ['페미닌', '러블리', '마켓 만족도 89%', '20대'],
-        shop_profie: 'https://shop-phinf.pstatic.net/20191107_60/1573103966356fPckB_PNG/10465508965383887_507755343.png?type=m120',
+      item: {
+        seller: {},
+        reviews: [{}],
       },
-      reviews: [
-        {
-          review_id: 'r1',
-          writer: 'ho***',
-          title: '만족해요',
-          content: '핏이 아주 잘 맞습니다. 대만족!',
-          like_count: 7,
-          created: '2021.12.04',
-
-        },
-      ],
     };
   },
+  created() {
+    this.getItem();
+  },
   methods: {
-
+    async getItem() {
+      const { data } = await ItemRepository.getItem(this.id);
+      this.item = data.item;
+    },
+    priceWidthComma(value) {
+      return String(value).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+    doesReviewImgExists(review) {
+      return Object.prototype.hasOwnProperty.call(review, 'img');
+    },
   },
   computed: {
+    display_discount_rate() {
+      const rate = ((this.item.original_price - this.item.price) / this.item.original_price) * 100;
+
+      return Number.prototype.toFixed.call(rate, 0);
+    },
   },
 };
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  margin: 2rem auto;
-  padding: 0;
-  max-width: 40rem;
-}
-body{
-  margin:0 auto;
-}
-ul{
-  margin:0;
-}
-li{
+.detail-wrap{
+  position:relative;
   box-shadow: none;
   margin:0;
   padding:0;
 }
-.detail{
-  position:relative;
-}
-.detail__data{
+
+.detail__data {
   display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
 }
-.detail__image{
+
+.detail__image {
   height: 400px;
 }
-.detail_main_image{
+.detail_main_image {
   object-fit: cover;
   width: 100%;
   height: 100%;
@@ -145,7 +188,7 @@ li{
   align-items: center;
   justify-content: center;
 }
-.detail__shop_image img{
+.detail__shop_image img {
   height: 42px;
   border-radius: 50%;
 }
@@ -167,7 +210,12 @@ li{
   width: 15%;
   padding: 5px;
 }
-.detail__actions{
+.detail-title{
+  padding: 20px;
+  font-size: 25px;
+  color: #737171;
+}
+.detail__actions {
   text-align: center;
   border-bottom: 1px solid #ddd;
   padding-bottom: 10px;
@@ -204,7 +252,7 @@ li{
 .description_p{
   margin: 30px 0px;
 }
-button{
+button {
   outline: none;
   font: inherit;
   cursor: pointer;
@@ -215,8 +263,20 @@ button{
   border-radius: 30px;
 }
 button:hover,
-button:active{
+button:active {
   background-color: #0a46b4;
   border-color: #0a46b4;
+}
+.detail-review-wrap{
+  margin-top: 20px;
+  margin-bottom: 100px;
+  background: #ddd;
+}
+.review-title{
+  font-size: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
