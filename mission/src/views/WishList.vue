af734b5f0cde9a037836c8ea0eb8ef011d6a7ad8
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-02-12 02:07:16 +0900

    Feat: "찜목록 페이지 구현 및 API연동"

---
+++ mission/src/views/WishList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id='wish-list-page' data-test='wish-list-page'>
+    <Header>찜목록</Header>
+
+    <main>
+      <div class="item-detail" data-test="item-list-page">
+        <Item
+        v-for="item in items"
+        :id="item.product_no"
+        :name="item.name"
+        :img="item.image"
+        :price="item.price"
+        :original_price="item.original_price"
+        :description="item.description"
+        :key="item.product_no"
+        ></Item>
+      </div>
+    </main>
+
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+// repository pattern으로 api 연동
+import RepositoryFactory from '@/repositories/RepositoryFactory';
+import Header from '../components/ItemList/Header.vue';
+import NavBar from '../components/ItemList/NavBar.vue';
+import Item from '../components/ItemList/Item.vue';
+
+const ItemRepository = RepositoryFactory.get('wishes');
+
+export default {
+  name: 'WishListPage',
+  components: { Header, Item, NavBar },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    async getItems() {
+      const { data } = await ItemRepository.get();
+      this.items = data.items;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
