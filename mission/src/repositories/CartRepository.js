7b4027ad48649b7bb73e84aebae050aa54e826b2
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-02-11 20:15:54 +0900

    Feat: "api연동할 reposiitory 생성"

---
+++ mission/src/repositories/CartRepository.js
@@ -0,0 +1,23 @@
+import Client from './Clients/AxiosClient';
+
+const resource = '/cart';
+
+export default {
+  get() {
+    return Client.get(`${resource}`);
+  },
+  getItem(id) {
+    return Client.get(`${resource}/${id}`);
+  },
+  create(payload) {
+    return Client.post(`${resource}`, payload);
+  },
+  update(payload, id) {
+    return Client.put(`${resource}/${id}`, payload);
+  },
+  delete(id) {
+    return Client.delete(`${resource}/${id}`);
+  },
+
+  // MANY OTHER ENDPOINT RELATED STUFFS
+};
