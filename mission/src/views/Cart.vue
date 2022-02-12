afaefd6380d7acab799ed90597984faf158c6d14
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2022-02-12 23:09:55 +0900

    Feat: "장바구니 vuex data 연동 "

--- mission/src/views/Cart.vue
+++ mission/src/views/Cart.vue
@@ -4,47 +4,34 @@
     <main>
       <div>
         <Item
-      v-for="item in items"
-      :key="item.productCode"
-      :name="item.name"
-      :price="item.price"
-      :img="item.image"
-      :original_price="item.original_price"
-      :description="item.description"
-    >
-    </Item>
+        v-for="item in storedCart"
+        :id="item.product_no"
+        :name="item.name"
+        :img="item.image"
+        :price="item.price"
+        :original_price="item.original_price"
+        :description="item.description"
+        :key="item.product_no"
+        ></Item>
       </div>
     </main>
-
     <NavBar/>
   </div>
 </template>
 
 <script>
-// repository pattern으로 api 연동
-import RepositoryFactory from '@/repositories/RepositoryFactory';
+import { mapGetters } from 'vuex';
 import Header from '../components/ItemList/Header.vue';
 import NavBar from '../components/ItemList/NavBar.vue';
 import Item from '../components/ItemList/Item.vue';
 
-const CartRepository = RepositoryFactory.get('carts');
-
 export default {
-  name: 'CartPage',
-  components: { Header, Item, NavBar },
-  data() {
-    return {
-      items: [],
-    };
+  name: 'OrderPage',
+  components: {
+    Header, Item, NavBar,
   },
-  created() {
-    this.getItems();
-  },
-  methods: {
-    async getItems() {
-      const { data } = await CartRepository.get();
-      this.items = data.cart_item;
-    },
+  computed: {
+    ...mapGetters(['storedCart']),
   },
 };
 </script>
